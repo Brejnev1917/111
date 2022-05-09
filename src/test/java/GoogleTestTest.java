480c8c3bb7f2b74a0a5c18faae8c3fddd486f419
brejnev1917 <brejnev1917@gmail.com>
2022-05-09 11:28:53 +0300

    Initial commit

---
+++ src/test/java/GoogleTestTest.java
@@ -0,0 +1,17 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+
+public class GoogleTestTest {
+    @Test
+    void successfulGoogleTestTest() {
+        open("https://www.google.com/");
+        $("[name=q]").setValue("selenide").pressEnter();
+        $("[id=search]").shouldHave(text("https://ru.selenide.org"));
+    }
+}
